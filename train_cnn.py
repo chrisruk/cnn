279576c865c993483ee0bdb8eaf140cc614b1c2c
chrisruk <chrisrichardsonuk@gmail.com>
2016-07-16 17:49:29 +0100

    remove whitespace

--- train_cnn.py
+++ train_cnn.py
@@ -45,13 +45,10 @@ for m in mod:
     for k in radioml.keys():
         if k[0] == m :
             for sig in range(len(radioml[k])):
-    
                 a = numpy.array(radioml[k][sig][0])[:, newaxis]
                 b = numpy.array(radioml[k][sig][1])[:, newaxis]
-
                 if k[1] not in data[k[0]]:
                     data[k[0]][k[1]] = []
-
                 data[k[0]][k[1]].append([a,b])
 
 X = []
@@ -62,26 +59,20 @@ mval = {}
 count = 0
 
 for m in mod:
-
     z = np.zeros((len(mod),))                                                                                                                                                                               
     z[count] = 1     
     mval[m] = z
-
     for snr in data[m]:
         dat = data[m][snr]
-    
         for d in dat[:len(dat)//2]:
             X.append(d)
             Y.append(z)
         for d in dat[len(dat)//2:]:
-
             if not snr in x:
                 x[snr] = []
                 y[snr] = []
-
             x[snr].append(d)
             y[snr].append(z)
-
     count += 1    
 
 network = input_data(shape=[None, 2, 128,1])
