a5dfa8431a9a81274f1b164b602e971282f2ffc1
chrisruk <chrisrichardsonuk@gmail.com>
2016-07-17 09:47:14 +0100

    save model

--- train_cnn.py
+++ train_cnn.py
@@ -25,7 +25,7 @@ from tflearn.data_utils import shuffle, to_categorical
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
-
+from tensor import *
 
 radioml = cPickle.load(open("2016.04C.multisnr.pkl",'rb'))
 
@@ -86,17 +86,20 @@ config = tf.ConfigProto(allow_soft_placement = True)
 sess = tf.Session(config = config)
 with tf.Session(config=config) as sess:
 #with tf.device('/gpu:0'):
-    network = input_data(shape=[None, 2, 128,1])
-    network = conv_2d(network, 64,[1,3], activation='relu')
-    network = conv_2d(network, 16,[2,3], activation='relu')
-    network = fully_connected(network, 128, activation='relu')
-    network = dropout(network, 0.5)
-    network = fully_connected(network, len(mod), activation='softmax')
+    network = input_data(shape=[None, 2, 128,1],name="inp")
+    network = conv_2d(network, 64,[1,3], activation='relu',name="conv1")
+    network = conv_2d(network, 16,[2,3], activation='relu',name="conv2")
+    network = fully_connected(network, 128, activation='relu',name="fully")
+    network = dropout(network, 0.5,name="drop1")
+    network = fully_connected(network, len(mod), activation='softmax',name="out")
     network = regression(network, optimizer='adam',
                      loss='categorical_crossentropy',
                      learning_rate=0.001)
     model = tflearn.DNN(network, tensorboard_verbose=0)
-    model.fit(X, Y, n_epoch=400, shuffle=True,show_metric=True, batch_size=1024)
+    model.fit(X, Y, n_epoch=1000, shuffle=True,show_metric=True, batch_size=1024)
+    model.save("cnn.ann")
+    #save_graph(sess,"/tmp/","saved_checkpoint","checkpoint_state","input_graph.pb","output_graph.pb")
+    
 
 for snr in sorted(x):
     gd = 0
